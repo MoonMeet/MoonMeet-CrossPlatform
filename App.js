e7be036a2045ead97dbc19aa04a7ac800996e406
AzizVirus <aziz07becha@gmail.com>
2021-06-18 18:41:49 +0100

    feat: added cpomponents folder and exported splash screen to the App.js

--- App.js
+++ App.js
@@ -1,41 +1,13 @@
 import React from "react";
 import { StyleSheet, View, Text, Image, StatusBar } from "react-native";
-
+import SplashScreen from "./components/SplashScreen";
 const App = () => {
   return (
-    <View style={styles.container}>
-     <StatusBar
-      animated={true}
-      backgroundColor="#FFFFFF"
-      barStyle={'dark-content'} />
-      <Image style={styles.logo} source={require('./src/logo.png')} />
-      <Text style={styles.bottom_text}>
-        Moon Meet
-      </Text>
+    <View>
+     <SplashScreen />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFFFFF',
-    padding: 8,
-  },
-  logo: {
-    height: 180,
-    width: 180,
-  },
-  bottom_text: {
-    position: 'absolute',
-    textAlign: 'center',
-    fontSize: 20,
-    bottom: 10,
-    color: '#566193',
-  },
-});
 
 export default App;
