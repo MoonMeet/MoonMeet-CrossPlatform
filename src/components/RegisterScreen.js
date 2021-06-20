f4edd83a563171d9332e26b7fcbb6292d4ba6417
Rayen Mark <rayensbai2@gmail.com>
2021-06-21 01:19:28 +0200

    dummy: dummy commit.

--- src/components/RegisterScreen.js
+++ src/components/RegisterScreen.js
@@ -1,39 +1,36 @@
 import React, { useState } from "react";
-import { SafeAreaView, StatusBar, StyleSheet, Text, View, Image, TextInput, Picker, Button } from "react-native";
+import { Alert, Button, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, View } from "react-native";
 import { COLORS, FONTS } from "../config/miscellaneous";
+import Modal from "react-native-modal";
 
 const RegisterScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <SafeAreaView nativeID={'container'} style={styles.container}>
+    <SafeAreaView nativeID={"container"} style={styles.container}>
       <StatusBar
         backgroundColor="#FFFFFF"
         barStyle={"dark-content"} />
-      <View nativeID={'top_view'} style={styles.top_bar}>
-        <Text nativeID={'top_text'} style={styles.top_text}>
+      <View nativeID={"top_view"} style={styles.top_bar}>
+        <Text nativeID={"top_text"} style={styles.top_text}>
           Sign Up
         </Text>
       </View>
 
-      <View nativeID={'top_view_divider'} style={styles.divider}>
+      <View nativeID={"top_view_divider"} style={styles.divider}>
       </View>
 
-      <View nativeID={'dummy_view'}>
+      <View nativeID={"dummy_view"}>
       </View>
-      <View nativeID={'country_holder'} style={styles.container} >
-
-
+      <View nativeID={"country_holder"} style={styles.container}>
         <View style={styles.row}>
-          <Picker style={styles.dropDown}>
-            <Picker.Item label="+216 Tunisia" value="+216" />
-            <Picker.Item label="(+33) France" value="+33" />
-          </Picker>
-
           <TextInput
             style={styles.phoneInput}
             placeholder="Enter your phone number"
-            type="numeric"
-          /></View>
-        <Button style={styles.submitBtn} title="Continue" />
+            type="numeric"/>
+        </View>
+        <Button style={styles.submitBtn}
+                title="Continue"
+                onPress={() => setModalVisible(!modalVisible)} />
       </View>
     </SafeAreaView>
   );
@@ -48,16 +45,16 @@ const styles = StyleSheet.create({
   },
   row: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
 
   },
   phoneInput: {
     flex: 1,
     margin: 15,
     height: 40,
-    width: '60%',
+    width: "60%",
     borderColor: COLORS.accent,
-    borderWidth: 1
+    borderWidth: 1,
   },
   submitBtn: {
     color: "#566193",
@@ -79,12 +76,12 @@ const styles = StyleSheet.create({
     fontFamily: FONTS.regular,
   },
   country_holder: {
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 16,
   },
   textCenter: {
-    textAlign: 'center',
-    justifyContent: 'center'
+    textAlign: "center",
+    justifyContent: "center",
   },
   loginImg: {
     width: 200,
@@ -94,9 +91,9 @@ const styles = StyleSheet.create({
   dropDown: {
     margin: 15,
     height: 40,
-    width: '40%',
+    width: "40%",
     borderColor: COLORS.accent,
-    borderWidth: 1
+    borderWidth: 1,
   },
 
 });
