238010d43df7013a786d1d61b710d1d4f6eb9c91
Rayen Mark <rayensbai2@gmail.com>
2021-06-25 16:06:18 +0200

    feat: CountriesList

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,12 +1,82 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import exampleJson from './example.json'
+import React, { Component } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import CountriesJson from "../assets/data/json/country-codes.json";
+import { COLORS, FONTS } from "../config/miscellaneous";
+import LoginScreen from "./LoginScreen";
+import { IconButton } from "react-native-paper";
+
+const MyList = CountriesJson;
+const loginScreenVar = LoginScreen;
+const SearchImage = require('../assets/images/search.png')
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: '3%',
+    paddingBottom: '3%',
+    backgroundColor: COLORS.primary,
+  },
+  top_bar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title_text: {
+    color: COLORS.accent,
+    fontFamily: FONTS.regular,
+    fontSize: 24,
+  },
+  country_text: {
+    color: COLORS.accent,
+    fontFamily: FONTS.regular,
+    fontSize: 20,
+    textAlign: "left",
+  },
+  dial_text: {
+    color: COLORS.darkGrey,
+    fontFamily: FONTS.regular,
+    fontSize: 20,
+    textAlign: "right",
+  },
+});
 
 export default class CountriesList extends Component {
+  constructor() {
+    super();
+  }
 
   render() {
     return (
-      {}
+      <View style={{
+        marginBottom: "5%",
+      }}>
+        <View style={styles.top_bar}>
+          <Text style={styles.title_text}>
+            Select your country
+          </Text>
+          <IconButton icon={SearchImage}
+                      color={'#999999'}
+                      size={24}
+                      style={{ paddingBottom: '0%'}}
+                      onPress={() => console.log('Pressed')}
+          />
+        </View>
+        <FlatList showsVerticalScrollIndicator={false} data={MyList} renderItem={({ item }) =>
+          <View style={styles.container}>
+            <Text onPress={() => {
+              loginScreenVar.UserSelectedCountry = "hello";
+            }}
+                  style={styles.country_text}>
+              {item.name}
+            </Text>
+            <Text style={styles.dial_text}>
+              {item.dial_code}
+            </Text>
+          </View>
+        }>
+        </FlatList>
+      </View>
     );
   }
 };
