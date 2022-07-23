e47d6969efd95735c0cde49f7376f6ae1ac0b460
SectionTN <rayensbai2@gmail.com>
2022-07-23 12:38:15 +0100

    feat: movin dev options into App component.

--- App.js
+++ App.js
@@ -2,20 +2,34 @@ import React, {useEffect} from 'react';
 import StackNavigator from './src/config/Stack';
 import {StatusBar} from 'react-native';
 import Toast from 'react-native-toast-message';
-import {enableFreeze} from 'react-native-screens';
 import {COLORS} from './src/config/Miscellaneous';
 import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
 import {ThemeContext} from './src/config/Theme/Context';
-import {MoonMeetDarkTheme, MoonMeetLightTheme} from './src/config/Theme/Theme';
 import {ThemeMMKV} from './src/config/MMKV/ThemeMMKV';
+import {MoonMeetDarkTheme, MoonMeetLightTheme} from './src/config/Theme/Theme';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
+import {initializeMMKVFlipper} from 'react-native-mmkv-flipper-plugin';
+import crashlytics from '@react-native-firebase/crashlytics';
+import analytics from '@react-native-firebase/analytics';
+import appCheck from '@react-native-firebase/app-check';
 
 /**
  * Enabling the experimental freeze of react-native-screens
  * enableFreeze(true);
  * Will enable this soon
- */
+ **/
+
+async function enableFirebaseTools() {
+  await crashlytics()?.setCrashlyticsCollectionEnabled(true);
+  await analytics()?.setAnalyticsCollectionEnabled(true);
+  await appCheck()?.activate('ignored', false);
+}
+
+if (__DEV__) {
+  initializeMMKVFlipper({default: ThemeMMKV});
+  enableFirebaseTools();
+}
 
 const App = () => {
   const [isThemeDark, setIsThemeDark] = React.useState(false);
