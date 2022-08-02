7b3c21ff1a4f49b79dcd8645a827b2678f71893b
SectionTN <rayensbai2@gmail.com>
2022-08-03 00:19:35 +0100

    feat: added appname.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import {name as MoonMeet} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(MoonMeet, () => App);
