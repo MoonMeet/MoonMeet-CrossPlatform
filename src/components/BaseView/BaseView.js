d1a44d01023a4d3238ff26d63afcf521d753a6d4
SectionTN <rayensbai2@gmail.com>
2022-08-03 00:20:01 +0100

    refactor: unused variable.

--- src/components/BaseView/BaseView.js
+++ src/components/BaseView/BaseView.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Keyboard, Pressable, SafeAreaView, StyleSheet} from 'react-native';
 import {COLORS} from '../../config/Miscellaneous';
-import {withTheme, useTheme} from 'react-native-paper';
+import {withTheme} from 'react-native-paper';
 import {ThemeContext} from '../../config/Theme/Context';
 
 const BaseView = ({children}) => {
-  const theme = useTheme();
   const {isThemeDark} = React.useContext(ThemeContext);
 
   const styles = StyleSheet.create({
