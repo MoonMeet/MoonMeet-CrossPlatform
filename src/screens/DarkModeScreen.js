8f2953343eda993128e9265131d2103570c8a5d6
SectionTN <rayensbai2@gmail.com>
2022-08-09 18:29:01 +0100

    refactor: remove unused characters ",".

--- src/screens/DarkModeScreen.js
+++ src/screens/DarkModeScreen.js
@@ -1,4 +1,4 @@
-import React, from 'react';
+import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Spacer from '../components/Spacer/Spacer';
 import MiniBaseView from '../components/MiniBaseView/MiniBaseView';
