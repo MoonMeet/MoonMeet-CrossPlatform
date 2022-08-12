e9d2055a37b6fb3c59452f3a7b375f8f18bd64ba
SectionTN <rayensbai2@gmail.com>
2022-08-12 23:32:29 +0100

    feat: update Q&A, fix:  ScrollView not scrolling.

--- src/components/Modals/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js
+++ src/components/Modals/FrequentlyAskedQuestions/FrequentlyAskedQuestions.js
@@ -1,5 +1,5 @@
 import React, {useCallback, useMemo} from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import {COLORS, FONTS} from '../../../config/Miscellaneous';
 import {
   BottomSheetBackdrop,
@@ -9,6 +9,7 @@ import {
   useBottomSheetSpringConfigs,
 } from '@gorhom/bottom-sheet';
 import {fontValue} from '../../../config/Dimensions';
+import {ScrollView} from 'react-native-gesture-handler';
 
 interface FAQInterface {
   sheetRef?: Ref | undefined;
@@ -69,34 +70,30 @@ const FrequentlyAskedQuestions = (props: FAQInterface) => {
           backgroundColor: COLORS.primaryLight,
         }}>
         <Text style={styles.headerText}>Frequently Asked Questions</Text>
-        <ScrollView
-          nestedScrollEnabled={true}
-          showsVerticalScrollIndicator={false}>
+        <ScrollView>
           <Text style={styles.subText}>Q&A 1: What is Moon Meet?</Text>
           <Text style={styles.subText}>
             Moon Meet is an instant messaging application that gives you a good
-            access to connect with friends, Make relationships, Share Stories...
+            access to connect with friends, make relationships and others.
           </Text>
           <Text style={styles.subText}>Q&A 2: Who it is for ?</Text>
           <Text style={styles.subText}>
-            Moon Meet is allowed for everyone who wanna chat and make
-            Relationships and new Friends and care about privacy.
+            Moon Meet is allowed for everyone above 13 years old.
           </Text>
           <Text style={styles.subText}>Q&A 3: Why Moon Meet?</Text>
           <Text style={styles.subText}>
-            Use Moon Meet to chat and communicate assured that all your data and
-            info are completely secret.
+            Because there is only few apps like it.
           </Text>
           <Text style={styles.subText}>Q&A: How i can get Moon Meet ?</Text>
           <Text style={styles.subText}>
             You can get Moon Meet by downloading the app from Play Store or
-            AppStore.
+            Github Release.
           </Text>
           <Text style={styles.subText}>
             Q&A 5: does Moon Meet has end-to-end Encryption ?
           </Text>
           <Text style={styles.subText}>
-            Future updates going to include this feature.
+            No, Future updates going to include this feature.
           </Text>
           <Text style={styles.subText}>
             Q&A 6: Is my information changeable ?
@@ -106,16 +103,18 @@ const FrequentlyAskedQuestions = (props: FAQInterface) => {
           </Text>
           <Text style={styles.subText}>Q&A 7: Can i delete my account ?</Text>
           <Text style={styles.subText}>
-            Yes, You can delete your Moon Meet account at anytime.
+            Yes, Moon Meet let you delete your account at any time but
+            unfortunately, it is not available at this time, it going to be
+            included soon.
           </Text>
           <Text style={styles.subText}>Q&A 8: Who can see me online ?</Text>
           <Text style={styles.subText}>
             By default, your active status is visible to everyone. You can hide
-            your online status from the settings anytime you want.
+            your active status from settings anytime you want.
           </Text>
           <Text style={styles.subText}>Q&A 9: Who is Behind Moon Meet ?</Text>
           <Text style={styles.subText}>
-            Behind Moon Meet, There is Rayen Sbai, Aziz Becha and our GitHub
+            Behind Moon Meet, There is Rayen Sbai A.K.A SectionTN and our GitHub
             Contributors.
           </Text>
           <Text style={styles.subText}>Q&A 10: Can i report a problem ?</Text>
@@ -127,8 +126,8 @@ const FrequentlyAskedQuestions = (props: FAQInterface) => {
             Q&A 11: Can i help to improve this Platform ?
           </Text>
           <Text style={styles.subText}>
-            Sure, If you have some good ideas you can open a pull request in our
-            GitHub repository and become one of the contributors.
+            Sure, you can open a pull request in our GitHub repository and
+            become one of the contributors.
           </Text>
           <Text style={styles.subText}>Q&A 12 : Who can see my profile ? </Text>
           <Text style={styles.subText}>
@@ -142,16 +141,17 @@ const FrequentlyAskedQuestions = (props: FAQInterface) => {
           <Text style={styles.subText}>
             Moon Meet collects public information about you like First Name,
             Last Name, Birth Date, Username, Bio, in shortly, the information
-            that you enter while and after doing the registration.
+            that you enter while and after doing the registration, but keep in
+            mind that we don't share or sell any of your data, you can check our
+            source code for that.
           </Text>
           <Text style={styles.subText}>
             Q&A 14: My phone has been stolen. Can support help me secure my
             account ?
           </Text>
           <Text style={styles.subText}>
-            Of course. If you encounter one of those problems like SIM or Phone
-            lost. You can contact our support team who can help you solve your
-            problem.
+            There's no information about this question for now, we'll update
+            this section and inform your in future
           </Text>
         </ScrollView>
       </BottomSheetView>
