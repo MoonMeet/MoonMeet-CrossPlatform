d0f6317d63ad1e0394308449f2298d014b8eebf3
SectionTN <rayensbai2@gmail.com>
2022-08-26 17:32:50 +0100

    feat: add OneSig,al to TOP JS file.

--- index.js
+++ index.js
@@ -3,7 +3,29 @@
  */
 
 import {AppRegistry} from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import App from './App';
 import {name as MoonMeet} from './app.json';
+import {ONESIGNAL_APP_ID} from './src/secrets/sensitive';
+
+OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+OneSignal.setNotificationWillShowInForegroundHandler(
+  notificationReceivedEvent => {
+    console.log(
+      'OneSignal: notification will show in foreground:',
+      notificationReceivedEvent,
+    );
+    let notification = notificationReceivedEvent.getNotification();
+    console.log('notification: ', notification);
+    const data = notification.additionalData;
+    console.log('additionalData: ', data);
+    notificationReceivedEvent.complete(notification);
+  },
+);
+
+OneSignal.setNotificationOpenedHandler(notification => {
+  console.log('OneSignal: notification opened:', notification);
+});
 
 AppRegistry.registerComponent(MoonMeet, () => App);
