b47f3a7529e02888710da8070de0d173c5ba392b
SectionTN <rayensbai2@gmail.com>
2022-08-28 14:43:07 +0100

    fallback: OneSignal notification.

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ import OneSignal from 'react-native-onesignal';
 import App from './App';
 import {name as MoonMeet} from './app.json';
 import {ONESIGNAL_APP_ID} from './src/secrets/sensitive';
-import notifee, {AndroidImportance} from '@notifee/react-native';
-import {COLORS} from './src/config/Miscellaneous';
+// import notifee, {AndroidImportance} from '@notifee/react-native';
+// import {COLORS} from './src/config/Miscellaneous';
 import {enableFreeze} from 'react-native-screens';
 
 /**
@@ -27,13 +27,13 @@ enableFreeze(true);
 OneSignal.setAppId(ONESIGNAL_APP_ID);
 OneSignal.setLanguage('en');
 
-notifee.createChannel({
+/**notifee.createChannel({
   id: 'messages',
   name: 'New Messages',
   lights: true,
   vibration: true,
   importance: AndroidImportance.HIGH,
-});
+});*/
 
 if (__DEV__) {
   OneSignal.setLogLevel(6, 0);
@@ -41,24 +41,22 @@ if (__DEV__) {
 
 OneSignal.setNotificationWillShowInForegroundHandler(
   notificationReceivedEvent => {
-    console.log(
-      'OneSignal: notification will show in foreground:',
-      notificationReceivedEvent,
-    );
+    if (__DEV__) {
+      console.log(
+        'OneSignal: notification will show in foreground:',
+        notificationReceivedEvent,
+      );
+    }
     let notification = notificationReceivedEvent.getNotification();
-    if (notificationReceivedEvent?.getNotification()?.body === 'New Message') {
-      notificationReceivedEvent.complete(null);
-      notifee.displayNotification({
+    /**notifee.displayNotification({
         title: 'New Message',
         body: 'A new message has been arrived.',
         android: {
           channelId: 'messages',
           color: COLORS.accentLight,
         },
-      });
-    } else {
-      notificationReceivedEvent.complete(notification);
-    }
+      });*/
+    notificationReceivedEvent.complete(notification);
   },
 );
 
