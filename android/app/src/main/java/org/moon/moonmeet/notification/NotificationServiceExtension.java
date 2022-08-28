d5dedf82e0ca7356642e37cbd547c9b22a7dc078
SectionTN <rayensbai2@gmail.com>
2022-08-28 02:10:06 +0100

    feat: class for NotificationServiceExtension.

---
+++ android/app/src/main/java/org/moon/moonmeet/notification/NotificationServiceExtension.java
@@ -0,0 +1,48 @@
+package org.moon.moonmeet.notification;
+  
+import android.content.Context;
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+
+import org.moon.moonmeet.*;
+import org.json.JSONObject;
+
+import com.onesignal.OSNotification;
+import com.onesignal.OSMutableNotification;
+import com.onesignal.OSNotificationReceivedEvent;
+import com.onesignal.OneSignal.OSRemoteNotificationReceivedHandler;
+
+import java.math.BigInteger;
+
+@SuppressWarnings("unused")
+public class NotificationServiceExtension implements OSRemoteNotificationReceivedHandler {
+
+    @Override
+    public void remoteNotificationReceived(Context context, OSNotificationReceivedEvent notificationReceivedEvent) {
+        OSNotification notification = notificationReceivedEvent.getNotification();
+
+        OSMutableNotification mutableNotification = notification.mutableCopy();
+        mutableNotification.setExtender(builder -> {
+
+            builder.setColor(new BigInteger("FF566193", 16).intValue());
+            Spannable spannableTitle = new SpannableString(notification.getTitle());
+            spannableTitle.setSpan(new ForegroundColorSpan(Color.BLACK), 0, notification.getTitle().length(), 0);
+            builder.setContentTitle(spannableTitle);
+
+            Spannable spannableBody = new SpannableString(notification.getBody());
+            spannableBody.setSpan(new ForegroundColorSpan(Color.BLACK), 0, notification.getBody().length(), 0);
+            builder.setContentText(spannableBody);
+
+            builder.setTimeoutAfter(30000);
+            return builder;
+        });
+        if (BuildConfig.DEBUG) {
+            JSONObject data = notification.getAdditionalData();
+            Log.i("OneSignal", "Received Notification Data: " + data);
+        }
+        notificationReceivedEvent.complete(mutableNotification);
+    }
+}
