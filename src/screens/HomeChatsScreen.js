c6c4837d49ba436d33c3956111563244d733946a
SectionTN <rayensbai2@gmail.com>
2022-09-01 14:28:58 +0100

    feat: loading.

--- src/screens/HomeChatsScreen.js
+++ src/screens/HomeChatsScreen.js
@@ -18,7 +18,7 @@ import {
 } from 'react-native';
 import {COLORS, FONTS} from '../config/Miscellaneous';
 import auth from '@react-native-firebase/auth';
-import {Avatar} from 'react-native-paper';
+import {ActivityIndicator, Avatar} from 'react-native-paper';
 import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import MiniBaseView from '../components/MiniBaseView/MiniBaseView';
 import MessagesList from '../components/HomeScreen/MessagesList';
@@ -57,6 +57,10 @@ const HomeChatsScreen = () => {
   const [storiesData, setStoriesData] = React.useState([]);
 
   const [myUID, setMyUID] = React.useState('');
+
+  const [storyLoading, setStoryLoading] = React.useState(true);
+
+  const [chatsLoading, setChatsLoading] = React.useState(true);
 
   const checkJwtKey = useCallback(
     currentJwtKey => {
@@ -165,7 +169,9 @@ const HomeChatsScreen = () => {
       .doc(auth()?.currentUser?.uid)
       .collection('discussions')
       .onSnapshot(collectionSnapshot => {
-        if (!collectionSnapshot?.empty) {
+        if (collectionSnapshot?.empty) {
+          setChatsData([]);
+        } else {
           let collectionDocs = collectionSnapshot?.docs?.map(subMap => ({
             ...subMap?.data(),
             id: subMap?.id,
@@ -175,9 +181,8 @@ const HomeChatsScreen = () => {
           ]);
           collectionDocs = reverse(collectionDocs);
           setChatsData(collectionDocs);
-        } else {
-          setChatsData([]);
         }
+        setChatsLoading(false);
       });
     return () => {
       chatSubscribe();
@@ -212,7 +217,9 @@ const HomeChatsScreen = () => {
     const storiesSubscribe = firestore()
       .collection('stories')
       .onSnapshot(collectionSnapshot => {
-        if (!collectionSnapshot?.empty) {
+        if (collectionSnapshot?.empty) {
+          setStoriesData([]);
+        } else {
           let collectionDocs = collectionSnapshot?.docs?.map(element => ({
             ...element?.data(),
             image:
@@ -227,9 +234,8 @@ const HomeChatsScreen = () => {
           ]);
           collectionDocs = reverse(collectionDocs);
           setStoriesData(collectionDocs);
-        } else {
-          setStoriesData([]);
         }
+        setStoryLoading(false);
       });
 
     return () => {
@@ -237,111 +243,132 @@ const HomeChatsScreen = () => {
     };
   }, []);
 
-  return (
-    <MiniBaseView>
-      <View style={styles.toolbar}>
-        <View style={styles.left_side}>
-          <Pressable
-            hitSlop={15}
-            onPress={() => {
-              navigation.navigate('settings');
-              updateUserActiveStatus();
-            }}>
-            <Avatar.Image
-              size={35.5}
-              source={
-                auth()?.currentUser?.photoURL
-                  ? {uri: auth()?.currentUser?.photoURL}
-                  : avatarURL
-                  ? {uri: avatarURL}
-                  : PurpleBackground
-              }
-              style={{
-                overflow: 'hidden',
-                marginRight: '-1%',
-              }}
-              theme={{
-                colors: {
-                  primary: COLORS.rippleColor,
-                },
-              }}
-            />
-          </Pressable>
+  if (storyLoading && chatsData) {
+    return (
+      <MiniBaseView>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <ActivityIndicator
+            animating={true}
+            size={'large'}
+            color={COLORS.accentLight}
+          />
         </View>
-        <View style={styles.mid_side}>
-          <Text style={styles.top_text}>Chats</Text>
-        </View>
-      </View>
-      <View
-        style={{
-          height: ITEM_HEIGHT,
-          width: '100%',
-        }}>
-        <StickyItemFlatList
-          itemWidth={ITEM_WIDTH}
-          itemHeight={ITEM_HEIGHT}
-          separatorSize={SEPARATOR_SIZE}
-          borderRadius={BORDER_RADIUS}
-          stickyItemWidth={STICKY_ITEM_WIDTH}
-          stickyItemHeight={STICKY_ITEM_HEIGHT}
-          stickyItemBackgroundColors={STICKY_ITEM_BACKGROUNDS}
-          stickyItemContent={props => (
-            <MoonStickyStoryView
-              {...props}
-              userAvatar={auth()?.currentUser?.photoURL}
-              tempAvatar={PurpleBackground}
-            />
-          )}
-          onStickyItemPress={undefined}
-          data={uniqBy(storiesData, 'uid')}
-          renderItem={({item}) => (
+      </MiniBaseView>
+    );
+  } else {
+    return (
+      <MiniBaseView>
+        <View style={styles.toolbar}>
+          <View style={styles.left_side}>
             <Pressable
-              style={{
-                height: ITEM_HEIGHT,
-                width: ITEM_WIDTH,
-                borderRadius: BORDER_RADIUS,
-                backgroundColor: COLORS.rippleColor,
-              }}
+              hitSlop={15}
               onPress={() => {
-                navigation?.navigate('story', {
-                  userUID: item?.uid,
-                  myUID: myUID,
-                });
+                navigation.navigate('settings');
+                updateUserActiveStatus();
               }}>
-              <Image
+              <Avatar.Image
+                size={35.5}
+                source={
+                  auth()?.currentUser?.photoURL
+                    ? {uri: auth()?.currentUser?.photoURL}
+                    : avatarURL
+                    ? {uri: avatarURL}
+                    : PurpleBackground
+                }
                 style={{
-                  backgroundColor: COLORS.white,
-                  width: ITEM_WIDTH,
-                  height: ITEM_WIDTH,
-                  borderRadius: BORDER_RADIUS,
-                  resizeMode: 'contain',
+                  overflow: 'hidden',
+                  marginRight: '-1%',
                 }}
-                source={{
-                  uri: item?.image ? item?.image : item?.avatar,
+                theme={{
+                  colors: {
+                    primary: COLORS.rippleColor,
+                  },
                 }}
               />
-              <Text
-                style={{
-                  position: 'absolute',
-                  width: '100%',
-                  textAlign: 'center',
-                  lineHeightight: 14,
-                  fontSize:
-                    Platform.OS === 'ios' ? fontValue(12) : fontValue(14),
-                  fontWeight: '500',
-                  color: COLORS.black,
-                  fontFamily: FONTS.regular,
-                  paddingHorizontal: SEPARATOR_SIZE * 2,
-                  transform: [{translateY: ITEM_HEIGHT / 2 + ITEM_HEIGHT / 4}],
-                }}>{`${item?.first_name}${' '}${item?.last_name}`}</Text>
             </Pressable>
-          )}
-        />
-      </View>
-      <Spacer height={heightPercentageToDP(0.5)} />
-      <MessagesList ListData={chatsData} />
-    </MiniBaseView>
-  );
+          </View>
+          <View style={styles.mid_side}>
+            <Text style={styles.top_text}>Chats</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: ITEM_HEIGHT,
+            width: '100%',
+          }}>
+          <StickyItemFlatList
+            itemWidth={ITEM_WIDTH}
+            itemHeight={ITEM_HEIGHT}
+            separatorSize={SEPARATOR_SIZE}
+            borderRadius={BORDER_RADIUS}
+            stickyItemWidth={STICKY_ITEM_WIDTH}
+            stickyItemHeight={STICKY_ITEM_HEIGHT}
+            stickyItemBackgroundColors={STICKY_ITEM_BACKGROUNDS}
+            stickyItemContent={props => (
+              <MoonStickyStoryView
+                {...props}
+                userAvatar={auth()?.currentUser?.photoURL}
+                tempAvatar={PurpleBackground}
+              />
+            )}
+            onStickyItemPress={undefined}
+            data={uniqBy(storiesData, 'uid')}
+            renderItem={({item}) => (
+              <Pressable
+                style={{
+                  height: ITEM_HEIGHT,
+                  width: ITEM_WIDTH,
+                  borderRadius: BORDER_RADIUS,
+                  backgroundColor: COLORS.rippleColor,
+                }}
+                onPress={() => {
+                  navigation?.navigate('story', {
+                    userUID: item?.uid,
+                    myUID: myUID,
+                  });
+                }}>
+                <Image
+                  style={{
+                    backgroundColor: COLORS.white,
+                    width: ITEM_WIDTH,
+                    height: ITEM_WIDTH,
+                    borderRadius: BORDER_RADIUS,
+                    resizeMode: 'contain',
+                  }}
+                  source={{
+                    uri: item?.image ? item?.image : item?.avatar,
+                  }}
+                />
+                <Text
+                  style={{
+                    position: 'absolute',
+                    width: '100%',
+                    textAlign: 'center',
+                    lineHeightight: 14,
+                    fontSize:
+                      Platform.OS === 'ios' ? fontValue(12) : fontValue(14),
+                    fontWeight: '500',
+                    color: COLORS.black,
+                    fontFamily: FONTS.regular,
+                    paddingHorizontal: SEPARATOR_SIZE * 2,
+                    transform: [
+                      {translateY: ITEM_HEIGHT / 2 + ITEM_HEIGHT / 4},
+                    ],
+                  }}>{`${item?.first_name}${' '}${item?.last_name}`}</Text>
+              </Pressable>
+            )}
+          />
+        </View>
+        <Spacer height={heightPercentageToDP(0.5)} />
+        <MessagesList ListData={chatsData} />
+      </MiniBaseView>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
