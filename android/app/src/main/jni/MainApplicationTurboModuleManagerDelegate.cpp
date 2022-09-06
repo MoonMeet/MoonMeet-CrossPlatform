34e503ec6e6dc66c64973fe5044698d1007856fd
SectionTN <rayensbai2@gmail.com>
2022-09-07 00:32:13 +0100

    chore: bump changes for files from RN70.

--- android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.cpp
+++ android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.cpp
@@ -22,15 +22,15 @@ void MainApplicationTurboModuleManagerDelegate::registerNatives() {
 
 std::shared_ptr<TurboModule>
 MainApplicationTurboModuleManagerDelegate::getTurboModule(
-    const std::string name,
-    const std::shared_ptr<CallInvoker> jsInvoker) {
+    const std::string &name,
+    const std::shared_ptr<CallInvoker> &jsInvoker) {
   // Not implemented yet: provide pure-C++ NativeModules here.
   return nullptr;
 }
 
 std::shared_ptr<TurboModule>
 MainApplicationTurboModuleManagerDelegate::getTurboModule(
-    const std::string name,
+    const std::string &name,
     const JavaTurboModule::InitParams &params) {
   return MainApplicationModuleProvider(name, params);
 }
