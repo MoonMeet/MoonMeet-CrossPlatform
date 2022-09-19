c5f414f9a57199daf4395eb058545a1eda765fea
SectionTN <rayensbai2@gmail.com>
2022-09-19 21:00:10 +0100

    babel: add more presets.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    '@babel/preset-flow',
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
   plugins: ['react-native-reanimated/plugin', 'babel-plugin-styled-components'],
 };
