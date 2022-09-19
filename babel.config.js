8f0d8c037c7b9bca4218380caa23fb96fa59ffbe
SectionTN <rayensbai2@gmail.com>
2022-09-19 21:37:14 +0100

    revert babel: config.

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    '@babel/preset-flow',
-    '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: ['react-native-reanimated/plugin', 'babel-plugin-styled-components'],
 };
