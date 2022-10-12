00b98c78419b291501c53b167fd4c4f5165a2ec3
SectionTN <rayensbai2@gmail.com>
2022-10-12 17:43:58 +0100

    refactor: remove unused styles.

--- src/screens/DiscoverPeopleScreen.js
+++ src/screens/DiscoverPeopleScreen.js
@@ -7,14 +7,14 @@
  */
 
 import React, {useEffect} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {COLORS, FONTS} from '../config/Miscellaneous';
+import {View} from 'react-native';
+import {COLORS} from '../config/Miscellaneous';
 import MiniBaseView from '../components/MiniBaseView/MiniBaseView';
 import {ActivityIndicator} from 'react-native-paper';
 import Spacer from '../components/Spacer/Spacer';
 import UserList from '../components/DiscoverPeopleScreen/UserList';
 import firestore from '@react-native-firebase/firestore';
-import {fontValue, heightPercentageToDP} from '../config/Dimensions';
+import {heightPercentageToDP} from '../config/Dimensions';
 
 const DiscoverPeopleScreen = () => {
   const [Loading, setLoading] = React.useState(true);
@@ -69,36 +69,4 @@ const DiscoverPeopleScreen = () => {
     </MiniBaseView>
   );
 };
-const styles = StyleSheet.create({
-  under_header: {
-    padding: '2%',
-    flexDirection: 'row',
-  },
-  left_side: {
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  mid_side: {
-    flex: 2,
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    alignItems: 'center',
-    fontSize: 18,
-    marginLeft: '2.5%',
-    marginRight: '2.5%',
-  },
-  toolbar: {
-    padding: '2%',
-    flexDirection: 'row',
-  },
-  toolbar_text: {
-    fontSize: fontValue(16),
-    paddingLeft: '2%',
-    paddingRight: '3%',
-    textAlign: 'center',
-    color: COLORS.black,
-    fontFamily: FONTS.regular,
-  },
-});
 export default DiscoverPeopleScreen;
