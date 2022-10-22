0d72b13254ca1dd4071b8bf3ad567086570d60b4
SectionTN <rayensbai2@gmail.com>
2022-10-22 11:56:48 +0100

    feat: improve ActivePeopleList.

--- src/components/HomeScreen/ActivePeopleList.js
+++ src/components/HomeScreen/ActivePeopleList.js
@@ -9,11 +9,12 @@
 import {FlatList, Pressable, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {COLORS, FONTS} from '../../config/Miscellaneous';
-import {Avatar} from 'react-native-paper';
+import {Avatar, Divider} from 'react-native-paper';
 import moment from 'moment';
 import {uniqBy} from 'lodash';
 import auth from '@react-native-firebase/auth';
 import {useNavigation} from '@react-navigation/native';
+import {fontValue} from '../../config/Dimensions';
 
 const ActivePeopleList = ({ListData}) => {
   const navigation = useNavigation();
@@ -21,8 +22,8 @@ const ActivePeopleList = ({ListData}) => {
   const listEmptyComponent = () => {
     return (
       <View style={styles.emptyView}>
-        <Text style={styles.heading}>No one active, yet.</Text>
-        <Text style={styles.subheading}>
+        <Text style={styles.heading('center', false)}>No one active, yet.</Text>
+        <Text style={styles.subheading('center', false, true)}>
           there's no one active at the moment.
         </Text>
       </View>
@@ -31,37 +32,40 @@ const ActivePeopleList = ({ListData}) => {
 
   const renderItem = ({item}) => {
     return (
-      <Pressable
-        android_ripple={{color: COLORS.rippleColor}}
-        style={styles.container}
-        onPress={() => {
-          if (item?.uid !== auth()?.currentUser?.uid) {
-            navigation.navigate('userProfile', {
-              uid: item?.uid,
-              cameFrom: 'others',
-            });
-          }
-        }}
-        onLongPress={() => {
-          if (item?.uid !== auth()?.currentUser?.uid) {
-            navigation?.navigate('chat', {item: item?.uid});
-          }
-        }}>
-        <View style={styles.left_side}>
-          <Avatar.Image
-            source={item?.avatar ? {uri: item?.avatar} : null}
-            size={52.5}
-          />
-        </View>
-        <View style={styles.mid_side}>
-          <Text style={styles.heading}>
-            {item?.first_name + ' ' + item?.last_name}
-          </Text>
-          <Text style={styles.subheading}>
-            {moment(item?.active_time?.toDate())?.calendar()}
-          </Text>
-        </View>
-      </Pressable>
+      <>
+        <Pressable
+          android_ripple={{color: COLORS.rippleColor}}
+          style={styles.container}
+          onPress={() => {
+            if (item?.uid !== auth()?.currentUser?.uid) {
+              navigation.navigate('userProfile', {
+                uid: item?.uid,
+                cameFrom: 'others',
+              });
+            }
+          }}
+          onLongPress={() => {
+            if (item?.uid !== auth()?.currentUser?.uid) {
+              navigation?.navigate('chat', {item: item?.uid});
+            }
+          }}>
+          <View style={styles.left_side}>
+            <Avatar.Image
+              source={item?.avatar ? {uri: item?.avatar} : null}
+              size={52.5}
+            />
+          </View>
+          <View style={styles.mid_side}>
+            <Text style={styles.heading}>
+              {item?.first_name + ' ' + item?.last_name}
+            </Text>
+            <Text style={styles.subheading}>
+              {moment(item?.active_time?.toDate())?.calendar()}
+            </Text>
+          </View>
+        </Pressable>
+        <Divider />
+      </>
     );
   };
 
@@ -99,21 +103,27 @@ const styles = StyleSheet.create({
     marginLeft: '2.5%',
     marginRight: '2.5%',
   },
-  heading: {
-    fontSize: 16,
-    textAlign: 'left',
-    color: COLORS.black,
-    fontFamily: FONTS.regular,
+  heading: (align, isRead) => {
+    return {
+      fontSize: 16,
+      textAlign: align,
+      color: COLORS.black,
+      opacity: isRead ? 0.6 : 1,
+      fontFamily: FONTS.regular,
+    };
   },
-  subheading: {
-    fontSize: 14,
-    paddingTop: '1%',
-    textAlign: 'left',
-    color: COLORS.black,
-    opacity: 0.4,
-    fontFamily: FONTS.regular,
+  subheading: (align, isMessage, isRead) => {
+    return {
+      fontSize: isMessage ? fontValue(14) : fontValue(11.5),
+      paddingTop: '1%',
+      textAlign: align,
+      color: COLORS.black,
+      opacity: isRead ? 0.6 : 1,
+      fontFamily: FONTS.regular,
+    };
   },
   emptyView: {
+    marginTop: 16 - 0.1 * 16,
     flex: 1,
     backgroundColor: COLORS.white,
     alignItems: 'center',
