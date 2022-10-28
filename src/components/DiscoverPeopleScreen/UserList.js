361250742abd7bcf5e82c0682cb20e45bb1058f9
SectionTN <rayensbai2@gmail.com>
2022-10-28 12:50:35 +0100

    refactor: extra console logging.

--- src/components/DiscoverPeopleScreen/UserList.js
+++ src/components/DiscoverPeopleScreen/UserList.js
@@ -34,7 +34,6 @@ const UserList = ({ListData}) => {
 
   const renderItem = ({item}) => {
     if (item?.uid !== auth()?.currentUser?.uid) {
-      console.log(item?.active_status + item?.active_time);
       return (
         <>
           <Pressable
