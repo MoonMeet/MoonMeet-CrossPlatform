30e558c83036377a52fc8529c6e6f2d3a1d57dd4
SectionTN <rayensbai2@gmail.com>
2023-01-17 16:43:34 +0100

    updateRN: iOS files / stil uncompleted, will update them soon.

--- ios/MoonMeet/AppDelegate.h
+++ ios/MoonMeet/AppDelegate.h
@@ -1,8 +1,8 @@
 #import <React/RCTBridgeDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
-
 @property (nonatomic, strong) UIWindow *window;
-
+@interface AppDelegate : RCTAppDelegate
 @end
